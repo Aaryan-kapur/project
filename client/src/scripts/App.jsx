c343016a634532ce4d91823298b70d07c447fbc0
Aaryan Kapur <ak6392@bennett.edu.in>
2019-09-01 20:25:04 +0530

    init yarn

---
+++ client/src/scripts/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Nav from "./Nav";
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <Nav />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
